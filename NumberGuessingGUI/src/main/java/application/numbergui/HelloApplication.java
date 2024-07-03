b82df3d0964dca33b47d96a01e5db189bb38414d
Ali Fathy <alifathy793@gmail.com>
2024-07-03 17:24:42 +0300

    Final

--- NumberGuessingGUI/src/main/java/application/numbergui/HelloApplication.java
+++ NumberGuessingGUI/src/main/java/application/numbergui/HelloApplication.java
@@ -3,9 +3,11 @@ package application.numbergui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     public static void main(String[] args) {
@@ -17,6 +19,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 540, 420);
         stage.setTitle("Guess The Number");
+        Image icon =new Image(Objects.requireNonNull(getClass().getResourceAsStream("/application/numbergui/dice.png")));
+        stage.getIcons().add(icon);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
