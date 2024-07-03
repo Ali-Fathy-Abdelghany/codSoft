799b630bad4ce9c385d5942ffbc61805e72e0979
Ali Fathy <alifathy793@gmail.com>
2024-07-03 16:36:54 +0300

    renamed file

--- NumberGuessingGUI/src/main/java/application/numbergui/HelloApplication.java
+++ NumberGuessingGUI/src/main/java/application/numbergui/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 420);
+        Scene scene = new Scene(fxmlLoader.load(), 540, 420);
         stage.setTitle("Guess The Number");
         stage.setResizable(false);
         stage.setScene(scene);
